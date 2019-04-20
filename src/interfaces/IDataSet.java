2c77f8da2a13b10b166d52fad58b55fc00f4db50
LuizFBR <38136395+LuizFBR@users.noreply.github.com>
2019-04-20 17:39:16 -0300

    Update IDataSet.java

--- src/interfaces/IDataSet.java
+++ src/interfaces/IDataSet.java
@@ -1,4 +1,14 @@
 package interfaces;
 
+public interface IDataSource {
+  public String getDataSource();
+  public void setDataSource(String dataSource);
+}
+
+public interface ITableProducer {
+  String[] requestAttributes();
+  String[][] requestInstances();
+}
+
 public interface IDataSet extends IDataSource, ITableProducer {
 }
